14596c552925a5865d8e9fd4b71f110a0f190323
Meo <albumeo1990@gmail.com>
2022-02-09 15:30:52 +0200

    fix: remove htmlSerializer

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,16 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
-import Prismic from "@prismicio/client";
 import { Client } from "../../prismic/prismic";
 
-const linkResolver = (doc: any) => {
-  // Pretty URLs for known types
-  if (doc.type === "blog") return "/post/" + doc.uid;
-  if (doc.type === "page") return "/" + doc.uid;
-  // Fallback for other types, in case new custom types get created
-  return "/doc/" + doc.id;
-};
+// const linkResolver = (doc: any) => {
+//   // Pretty URLs for known types
+//   if (doc.type === "blog") return "/post/" + doc.uid;
+//   if (doc.type === "page") return "/" + doc.uid;
+//   // Fallback for other types, in case new custom types get created
+//   return "/doc/" + doc.id;
+// };
 
 export async function getStaticPaths() {
   return {
